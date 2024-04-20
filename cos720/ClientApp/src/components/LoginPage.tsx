fd4b5300a7eea890836633780bb850897d24738d
MichaelLeForestier <m.leforestier1@gmail.com>
2024-04-20 12:26:29 +0200

    url

--- cos720/ClientApp/src/components/LoginPage.tsx
+++ cos720/ClientApp/src/components/LoginPage.tsx
@@ -46,7 +46,7 @@ const LoginPage: React.FC<LoginPageProps> = () => {
   const handleLogin = async () => {
     try {
       
-      const response = await axiosInstance.post('https://mikesprojcos720-v1-wandering-grass-8221.fly.dev//api/User/login', {
+      const response = await axiosInstance.post('https://mikesprojcos720-v1-winter-shape-4532.fly.dev/api/User/login', {
         email,
         password,
         
